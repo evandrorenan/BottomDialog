62466a0298fbd858eb3ac79612af1850ea91b758
evandrorenan <renan3006@hotmail.com>
2021-10-08 08:32:47 -0300

    source files

---
+++ app/src/main/java/br/com/evandrorenan/bottomdialog/FirstFragment.kt
@@ -0,0 +1,33 @@
+package br.com.evandrorenan.bottomdialog
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+            val modal =  SecondFragment()
+            modal.show(this.requireActivity().supportFragmentManager, "show_modal")
+        }
+    }
+}
